b8046644e5d46eb6a62d6b44c80505ab115b6749
Angela Donati <angeladonati93@gmail.com>
2022-01-24 15:17:32 -0500

    update formatting and readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # work-day-planner
+
+This project is a simple work day scheduler made with HTML, CSS/Bootstrap, and Jquery. The current time and date is displayed on the top header and has time slots from 9AM to 5PM where you can save your desired todo tasks by using the green button at the end of the row. Tasks are saved using local storage. A 'Clear All Tasks' button is included in the header which allows you to clear your entire scheduled tasks from the text areas and from local storage.
+
+
+## Installation
+
+Clone the repository [here](https://github.com/a-donati/work-day-planner)
+
+[Link to deployed page](https://a-donati.github.io/work-day-planner/)
+
+## Usage
+
+ ![Work-day-planner includes current date and time on the header, a clear tasks button, and textareas with save buttons.](https://user-images.githubusercontent.com/94128491/150857894-7d2b9fe7-c912-43d9-b772-6e27171e5ef0.png)
+
+  
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+## License
+
+Licensed under the MIT license.
+<br>
+<details>
+<summary>License Text</summary>
+<br>
+Copyright (c) [2020]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+</details>
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,15 @@
-let currentDay = $('#currentDay');
-let $divContainer = $('.container');
-let header = $('header');
-let clearBtnEl = $('.clearBtn');
-
-console.log(moment().hour);
+let currentDay = $("#currentDay");
+let $divContainer = $(".container");
+let header = $("header");
+let clearBtnEl = $(".clearBtn");
 
 // hours that will appear on calendar
-let hoursArray = [9,10,11,12,13,14,15,16,17];
+let hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // using moment.js to display the current date
 displayTime();
 function displayTime() {
-    var rightNow = moment().format('dddd, MMM Do, YYYY [at] hh:mm:ss A');
+    var rightNow = moment().format("dddd, MMM Do, YYYY [at] hh:mm:ss A");
     currentDay.text(rightNow);
 }
 // setInterval to update time every second
@@ -20,64 +18,62 @@ setInterval(displayTime, 1000);
 // using IIFE for fun ~ probably unecessary
 (function printCalendar() {
     // iterate through hoursArray length to create rows
-    for(let i=0; i < hoursArray.length; i++) {
+    for (let i = 0; i < hoursArray.length; i++) {
         let rowEl = $("<row>");
-        rowEl.addClass('row time-block');
+        rowEl.addClass("row time-block");
         // append created rows to container element
         $divContainer.append(rowEl);
 
         // create column to display hour
         let hourEl = $("<col>");
-        hourEl.addClass('col-1 hour');
+        hourEl.addClass("col-1 hour");
         // set inner hourEl text to display hour(hoursArray[i]) by using moment.js, hA format will set display as hour + am/pm
         hourEl.text(moment().set("hour", hoursArray[i]).format("hA"));
         rowEl.append(hourEl);
 
         // create textarea
-        let textareaEl = $('<textarea>');
-        textareaEl.addClass('col-10 description');
+        let textareaEl = $("<textarea>");
+        textareaEl.addClass("col-10 description");
         //set each textarea HTML data attribute to 'data-hour=hoursArray[i]'
-        textareaEl.data('hour', hoursArray[i]);
-        
+        textareaEl.data("hour", hoursArray[i]);
+
         // setting class styles to the text area depending on the current time
-        if(moment().hour() > hoursArray[i]) {
-            textareaEl.addClass('past');
-        } else if(moment().hour() < hoursArray[i]) {
-            textareaEl.addClass('future')
+        if (moment().hour() > hoursArray[i]) {
+            textareaEl.addClass("past");
+        } else if (moment().hour() < hoursArray[i]) {
+            textareaEl.addClass("future");
         } else {
-            textareaEl.addClass('present');
+            textareaEl.addClass("present");
         }
         // if localStorage != null, display ls text within textarea
-        if(localStorage.getItem(hoursArray[i])) {
+        if (localStorage.getItem(hoursArray[i])) {
             let savedText = localStorage.getItem(hoursArray[i]);
             textareaEl.text(savedText);
         }
         rowEl.append(textareaEl);
 
         // create save buttons
-        let saveBtn = $('<button>');
-        saveBtn.addClass('col-1 saveBtn fas fa-check-circle');
+        let saveBtn = $("<button>");
+        saveBtn.addClass("col-1 saveBtn fas fa-check-circle");
         rowEl.append(saveBtn);
 
         // when button is clicked, textarea is cleared and localstorage is wiped
-        clearBtnEl.on('click', (e)=> {
+        clearBtnEl.on("click", (e) => {
             e.preventDefault();
-            textareaEl.val('')
+            textareaEl.val("");
             localStorage.clear();
-        })
-        
+        });
     }
-})()    
+})();
 
-$divContainer.on('click', 'button', saveTask)
+$divContainer.on("click", "button", saveTask);
 function saveTask(e) {
-    e.preventDefault()
-    // using previousElementsibling to obtain the value from textarea when clicking the save button 
+    e.preventDefault();
+    // using previousElementsibling to obtain the value from textarea when clicking the save button
     console.log($(this.previousElementSibling));
     let task = $(this.previousElementSibling).val();
-    let workDayHour = $(this.previousElementSibling).data('hour');
+    let workDayHour = $(this.previousElementSibling).data("hour");
     console.log(task);
     console.log(workDayHour);
     localStorage.setItem(workDayHour, task);
 }
-
